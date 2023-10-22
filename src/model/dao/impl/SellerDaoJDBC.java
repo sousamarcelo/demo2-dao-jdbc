a2e3c6723ed838a6b8ad11eeea7c15c48f2e08c2
sousamarcelo <marcelo.sousa@aluno.faculdadeimpacta.com.br>
2023-10-21 21:39:53 -0300

    findAll implementation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -89,14 +89,50 @@ public class SellerDaoJDBC implements SellerDao {
 	private Department instantiateDepartment(ResultSet rs) throws SQLException {
 		Department dep = new Department(); 					//Criando um objeto Departmento que sera populado com os dado do banco
 		dep.setId(rs.getInt("DepartmentId")); 				//acessando a coluna correspondente ao Id do departamento no ResultSet rs e setando no objeto Department criado acima
-		dep.setName(rs.getString("Name")); 					//acessando a coluna correspondente ao Name do departamento no ResultSet rs e setando no objeto Department criado acima
+		dep.setName(rs.getString("DepName")); 					//acessando a coluna correspondente ao Name do departamento no ResultSet rs e setando no objeto Department criado acima
 		return dep;
 	}
 
 	@Override
 	public List<Seller> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		try {
+								
+			st = conn.prepareStatement(
+					"SELECT seller.*,department.Name as DepName "
+					+ "FROM seller INNER JOIN department "
+					+ " ON seller.DepartmentId = department.Id "					
+					+ "ORDER BY Name"					
+					);
+						
+			rs = st.executeQuery();
+																								//tranformando os dados de tabela em objetos relacionados
+			List<Seller> list = new ArrayList<Seller>();
+			Map<Integer, Department> map  = new HashMap<Integer, Department>();					// controle para não deixar criar um novo objeto de departamente para cada Vendedor, o certo e sempre utilizar o mesmo objeto
+						
+			while (rs.next()) {
+				
+				Department dep = map.get(rs.getInt("DepartmentId"));
+				
+																								//com esse if controla-se a instanciação do departamento, que se será instanciado se ainda não existir uma instancia
+				if (dep == null) {
+					dep = instantiateDepartment(rs);  											//criado metodo a parte para instanciação e adidiação dos dados no objeto, assim o codigo fica menos poluido
+					map.put(rs.getInt("DepartmentId"), dep);
+				}		
+													
+				Seller obj = instantiateSeller(rs, dep); 										//criado metodo a parte para instanciação e adidiação dos dados no objeto, assim o codigo fica menos poluido				
+				list.add(obj);
+							
+			}
+			return list;
+			
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		} finally {
+			DB.closeStatement(st); 
+			DB.closeResultSet(rs);
+		}
 	}
 
 	@Override
