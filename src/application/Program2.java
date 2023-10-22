6fc80c8c80560e8bbeb7c9e25a102e5015b812d9
sousamarcelo <marcelo.sousa@aluno.faculdadeimpacta.com.br>
2023-10-21 23:13:29 -0300

    Department: findAll implementation

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -1,10 +1,12 @@
 package application;
 
+import java.util.List;
 import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.DepartmentDao;
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program2 {
 
@@ -17,7 +19,11 @@ public class Program2 {
 		Department department = departmenteDao.findById(4);	
 		System.out.println(department);
 		
-		
+		System.out.println("\n=== TESTE 2: Departmente findAll ===");		
+		List<Department> list = departmenteDao.findAll();
+		for(Department obj : list) {
+			System.out.println(obj);
+		}
 		
 		
 		sc.close();
--- src/model/dao/impl/DepartmentDaoJDBC.java
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import db.DB;
@@ -79,14 +80,33 @@ public class DepartmentDaoJDBC implements DepartmentDao {
 
 	@Override
 	public List<Department> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		
+		try {
+			
+			String sql = "SELECT * FROM department ORDER BY department.Name";
+			
+			st = conn.prepareStatement(sql);
+			
+			rs = st.executeQuery();
+			
+			List<Department> list = new ArrayList<Department>();
+			
+			while (rs.next()) {
+				Department dep = instantiateDepartmente(rs);
+				list.add(dep);				
+			}
+			return list;
+			
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		} finally {
+			DB.closeResultSet(rs);
+			DB.closeStatement(st);
+		}
 	}
 
-	@Override
-	public List<Department> findByDepartment(Department department) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	
 
 }
